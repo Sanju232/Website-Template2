65124e4db2186fda033d95064a46a36deaeb5f7f
Sanju Kumar <122269636+Sanju232@users.noreply.github.com>
2025-05-31 10:12:37 +0530

    Add files via upload

---
+++ Project n2/app.js
@@ -0,0 +1,10 @@
+  document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      if (navbarCollapse.classList.contains('show')) {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: true
+        });
+      }
+    });
+  });
